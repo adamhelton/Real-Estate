05fac83c31587583ef7d0cb6e1907cad04a76b4e
Adam Helton <helton93@protonmail.com>
2019-10-14 21:16:56 -0400

    print content with parser

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ from bs4 import BeautifulSoup
 r = requests.get("http://www.pyclass.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/", headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
 c = r.content
 
+soup = BeautifulSoup (c, 'html.parser')
+print(soup.prettify())
