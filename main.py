2e367eb99e065939f2de2d8250cac0cad343beb3
Adam Helton <helton93@protonmail.com>
2019-10-14 21:09:37 -0400

    imports

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-this is the begining
+import requests
+from bs4 import BeautifulSoup
+
+
+
+
+
+
+
+# r = requests.get("http://www.pyclass.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/", headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
