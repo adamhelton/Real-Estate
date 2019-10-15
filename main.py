541433933ece69fd98fac3c766fa1957c4063885
Adam Helton <helton93@protonmail.com>
2019-10-14 21:13:07 -0400

    added requests and content

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
+r = requests.get("http://www.pyclass.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/", headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
+c = r.content
 
-
-
-
-
-
-# r = requests.get("http://www.pyclass.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/", headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
