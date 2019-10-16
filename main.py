f502f208c0f21abd5ed73d5229e12548b2aef23b
Adam Helton <helton93@protonmail.com>
2019-10-16 16:10:27 -0400

    added addresses output into for loop

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ c = r.content
 soup = BeautifulSoup (c, 'html.parser')
 
 all = soup.find_all("div", {"class":"propertyRow"})
-(all[0].find("h4", {"class":"propPrice"}).text.replace("/n", ""))
+all[0].find("h4", {"class":"propPrice"}).text.replace("/n", "").replace(" ", "")
 
 for item in all:
     print(item.find("h4", {"class", "propPrice"}).text.replace("/n", "").replace(" ", ""))
+    print(item.find_all("span",{"class", "propAddressCollapse"})[0])
