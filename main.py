0062ab149177021d98900e6e567cd30ee2027483
Adam Helton <helton93@protonmail.com>
2019-10-17 18:58:15 -0400

    Added pandas to convert to csv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas
 
 r = requests.get("http://www.pyclass.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/", headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
 c = r.content
@@ -9,36 +10,40 @@ soup = BeautifulSoup (c, 'html.parser')
 all = soup.find_all("div", {"class":"propertyRow"})
 all[0].find("h4", {"class":"propPrice"}).text.replace("/n", "").replace(" ", "")
 
+l = []
 for item in all:
     d = {}
     d["Address"] = item.find_all("span",{"class", "propAddressCollapse"})[0].text
     d["Locality"] = item.find_all("span",{"class", "propAddressCollapse"})[1].text
     d["Price"] = item.find("h4", {"class", "propPrice"}).text.replace("/n", "").replace(" ", "")
-    
-    try:
-        print(item.find("span", {"class", "infoBed"}).find("b").text)
-    except:
-        print(None)
 
     try:
-        print(item.find("span", {"class", "infoSqFt"}).find("b").text)
+        d["Beds"] = item.find("span", {"class", "infoBed"}).find("b").text
     except:
-        print(None)
+        d["Beds"] = None
 
     try:
-        print(item.find("span", {"class", "infoValueFullBath"}).find("b").text)
+        d["Area"] = item.find("span", {"class", "infoSqFt"}).find("b").text
     except:
-        print(None)
+        d["Area"] = None
 
     try:
-        print(item.find("span", {"class", "infoValueHalfBath"}).find("b").text)
+        d["Full Baths"] = item.find("span", {"class", "infoValueFullBath"}).find("b").text
     except:
-        print(None)
+        d["Full Baths"] = None
+
+    try:
+        d["Half Bath"] = item.find("span", {"class", "infoValueHalfBath"}).find("b").text
+    except:
+        d["Half Bath"] = None
+
     for column_group in item.find_all("div", {"class", "columnGroup"}):
         #print(column_group)
         for feature_group, feature_name in zip(column_group.find_all("span", {"class":"featureGroup"}),column_group.find_all("span",{"class":"featureName"})):
             # print(feature_group.text, feature_name.text) USe this if you want all the feature info
             if "Lot Size" in feature_group.text:
-                print(feature_name.text)
+                d["Lot Size"] = feature_name.text
 
-    print(" ")
+l.append(d)
+df = pandas.DataFrame(l)
+df.to_csv("Output.csv")
